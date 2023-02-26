5cfaeef1ca76b1af06a40ac6f8bb8e7674861b18
Manuel Santiago Gómez Mancipe <55415338+xManuelGMx@users.noreply.github.com>
2023-02-25 23:47:49 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -8,14 +8,14 @@ function fullscreen() {
     }
 }
 
-var grade = 0;
+let grade = 0;
 
 function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
+    let s = date.getSeconds(); // 0 - 59
+    let session = "AM";
 
     
     
@@ -32,15 +32,92 @@ function showTime(){
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s + " " + session;
+    let time = h + ":" + m + ":" + s + " " + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
 
     setTimeout(showTime, 1000);
     
-    document.querySelector('.clock').style.webkitFilter = 'hue-rotate('+ grade +'deg)';
+    document.querySelector('.reloj').style.webkitFilter = 'hue-rotate('+ grade +'deg)';
     grade += 15;
     
+    let year = date.getFullYear();
+    let month = "enero";
+
+    switch(date.getMonth() + 1) {
+      case 1:
+        month = "enero";
+        break;
+      case 2:
+        month = "febrero";
+        break;
+      case 3:
+        month = "marzo";
+        break;
+      case 4:
+        month = "abril";
+        break;
+      case 5:
+        month = "mayo";
+        break;
+      case 6:
+        month = "junio";
+        break;
+      case 7:
+        month = "julio";
+        break;
+      case 8:
+        month = "agosto";
+        break;
+      case 9:
+        month = "septiembre";
+        break;
+      case 10:
+        month = "octubre";
+        break;
+      case 11:
+        month = "noviembre";
+        break;
+      case 12:
+        month = "diciembre";
+        break;
+      
+      default:
+        month = "error :c";
+    }
+    let day = date.getDate();
+    let fecha = day + " de " + month + " de " + year;
+    let weekDay = "Lunes";
+    
+    switch(date.getDay() + 1) {
+      case 1:
+        weekDay = "Domingo";
+        break;
+      case 2:
+        weekDay = "Lunes";
+        break;
+      case 3:
+        weekDay = "Martes";
+        break;
+      case 4:
+        weekDay = "Miércoles";
+        break;
+      case 5:
+        weekDay = "Jueves";
+        break;
+      case 6:
+        weekDay = "Viernes";
+        break;
+      case 7:
+        weekDay = "Sábado";
+        break;
+
+      default:
+        weekDay = "error :c";
+    }
+    
+    document.querySelector('#fecha').textContent = fecha;
+    document.querySelector('#dia').textContent = weekDay;
 }
 
 showTime();
